1eae3db7bc97c6be59a63320b8fb90afb2c5c79a
Robinson D. S. Santos <robinsondssantos@gmail.com>
2020-03-12 17:38:38 -0300

    improve notification

--- src/app/models/Notification.js
+++ src/app/models/Notification.js
@@ -3,10 +3,16 @@ import Sequelize, { Model } from 'sequelize'
 class Notification extends Model {
 
   static init(sequelize) {
+    // https://zenviasms.docs.apiary.io/#introduction/autenticacao
     super.init(
       {
-        recipientPhone: Sequelize.STRING,
-        msg: Sequelize.STRING
+        from: Sequelize.STRING, // Name of semder
+        to: Sequelize.STRING, // Number - required
+        msg: Sequelize.STRING, // required
+        schedule: Sequelize.DATE, // 2014-07-18T02:01:23
+        id: Sequelize.STRING, // string
+        callbackOption: Sequelize.STRING, // enum
+        aggregateId: Sequelize.STRING // number
       },
       {
         sequelize
